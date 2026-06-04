7adf481a8ee5a49adb40f5e06471de39b0255bfb
Pradhiksha M <23cb040@drngpit.ac.in>
2026-06-04 17:36:48 +0530

    Fix ALLOWED_HOSTS for Render deployment

--- config/settings/production.py
+++ config/settings/production.py
@@ -5,7 +5,10 @@ from .base import env
 
 # GENERAL
 SECRET_KEY = env("DJANGO_SECRET_KEY")
-ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=[".onrender.com"])
+ALLOWED_HOSTS = env.list(
+    "DJANGO_ALLOWED_HOSTS",
+    default=["traceu-app.onrender.com", ".onrender.com"]
+)
 
 DEBUG = False
 
