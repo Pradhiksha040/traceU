36313b728db7770a97e5146677abcaae3618fe4c
Pradhiksha M <23cb040@drngpit.ac.in>
2026-06-04 17:11:05 +0530

    disable redis for render deployment

--- config/settings/production.py
+++ config/settings/production.py
@@ -13,13 +13,8 @@ DATABASES["default"]["CONN_MAX_AGE"] = env.int("CONN_MAX_AGE", default=60)
 # CACHE (Redis)
 CACHES = {
     "default": {
-        "BACKEND": "django_redis.cache.RedisCache",
-        "LOCATION": env("REDIS_URL"),
-        "OPTIONS": {
-            "CLIENT_CLASS": "django_redis.client.DefaultClient",
-            "IGNORE_EXCEPTIONS": True,
-        },
-    },
+        "BACKEND": "django.core.cache.backends.locmem.LocMemCache",
+    }
 }
 
 # SECURITY
